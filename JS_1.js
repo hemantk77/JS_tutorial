11109061eacd2c7dfec85a7dcc52de62eac4fae5
hemantk77 <hemant11uk@gmail.com>
2025-08-17 00:16:14 +0200

    JS 1 Practice set complete

--- JS_1.js
+++ JS_1.js
@@ -1,1 +1,18 @@
-console.log("Hemant Kumar");
+const product = {
+    productName : "Parker Jotter",
+    price : 270,
+    colour : "black",
+    type : "Ball pen",
+    rating : 4
+};
+
+console.log(product);
+
+const profile = {
+    username : "coolguyhk",
+    isFollow : true,
+    followers : 20,
+    following: 30
+};
+
+console.log(profile);
